d01380bae90b470fbf109ec37d6654ce4f8bb534
Kaustubh <kaustubhsingh37@gmail.com>
2024-01-01 22:16:32 +0530

    pivot search array

---
+++ src/arrays/BinarySearch/pivotSearch.java
@@ -0,0 +1,82 @@
+package arrays.BinarySearch;
+
+public class pivotSearch {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(nums, target));
+
+    }
+
+
+    static int search(int[] arr, int target) {
+        int pivot = pivot(arr);
+
+
+        //if you did not  found a pivot , it means the array is not rotated
+        if (pivot == -1) {
+            // just do normal binary search
+            return binarySearch(arr, target, 0, arr.length - 1);
+        }
+
+        // if you are able to find the pivot we have two ascending sorted array
+        // three cases arises
+        if (arr[pivot] == target) {
+            return pivot;
+        }
+
+        if (target >= arr[0]) {
+            return binarySearch(arr, target, 0, pivot - 1);
+        }
+
+        return binarySearch(arr, target, pivot + 1, arr.length - 1);
+
+    }
+
+    static int pivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // 4 cases
+
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid;
+            }
+
+            if (arr[start] >= arr[mid]) {
+                end = mid - 1;
+            }
+            if (arr[start] <= arr[mid]) {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+}
+
+
