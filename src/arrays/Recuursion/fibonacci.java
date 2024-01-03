9f69ab3ad76ece41c5a382258e6f9bfc637cd092
Kaustubh <kaustubhsingh37@gmail.com>
2024-01-03 21:52:31 +0530

    started recurrsion

---
+++ src/arrays/Recuursion/fibonacci.java
@@ -0,0 +1,18 @@
+package arrays.Recuursion;
+
+public class fibonacci {
+    public static void main(String[] args) {
+
+        System.out.println(fibo(3));
+
+    }
+
+    static int fibo(int n) {
+        // base condition
+        if (n < 2) {
+            return n;
+        }
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
