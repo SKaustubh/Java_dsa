3b1ff0ea4b1aa05b3e4ffb6fc50ee7ceba1d2bf2
Kaustubh <kaustubhsingh37@gmail.com>
2024-02-28 22:47:18 +0530

    subsequences video 7+all the patterns sum and counter for number of subsequences

--- src/recurrsion/sumOfSubSwquences.java
+++ src/recurrsion/sumOfSubSwquences.java
@@ -18,10 +18,18 @@ public class sumOfSubSwquences {
         ArrayList<Integer> ds = new ArrayList<>();
         int sum = 2;
         fn(0, ds, 0, sum, arr, n);
+        System.out.println("second output:-any one subsquence");
+        boolean bs = oneSubSequencePrint(0, ds, 0, sum, arr, n);
+        System.out.println(bs);
+
+        System.out.println("third output:-number of subsequences");
+        int k = subsequencesumK(0, 0, sum, arr, n);
+        System.out.println(k);
         sc.close();
 
     }
 
+    // function to print all the subsequences equal to sum
     static void fn(int ind, ArrayList<Integer> ds, int s, int sum, int[] arr, int n) {
         if (ind == n) {
             if (s == sum) {
@@ -44,5 +52,48 @@ public class sumOfSubSwquences {
         fn(ind + 1, ds, s, sum, arr, n); // delete
 
 
+    }
+
+    // function to print only one subsequences equal to sum
+
+    static boolean oneSubSequencePrint(int ind, ArrayList<Integer> ds, int s, int sum, int[] arr, int n) {
+        if (ind == n) {
+            if (s == sum) {
+
+                for (Integer it : ds) {
+                    System.out.print(it + " ");
+                }
+                System.out.println();
+                return true;
+
+            }
+            return false;
+        }
+
+        ds.add(arr[ind]);
+        s += arr[ind];
+        if (oneSubSequencePrint(ind + 1, ds, s, sum, arr, n) == true) return true; //take
+        s -= arr[ind];
+        ds.remove(ds.size() - 1);
+        if (oneSubSequencePrint(ind + 1, ds, s, sum, arr, n) == true) return true;
+        return false;// delete
+    }
+
+    // number of subsequences
+    static int subsequencesumK(int ind, int s, int sum, int[] arr, int n) {
+        if (ind == n) {
+            if (s == sum) {
+                return 1;
+            }
+            return 0;
+        }
+
+
+        s += arr[ind];
+        int l = subsequencesumK(ind + 1, s, sum, arr, n); //take
+        s -= arr[ind];
+
+        int r = subsequencesumK(ind + 1, s, sum, arr, n);
+        return l + r;// delete
     }
 }
