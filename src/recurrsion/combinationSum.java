53abfc4672b12f4e3cfaadca97d556a824c857c4
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-01 19:52:12 +0530

    combination sum -type 2

--- src/recurrsion/combinationSum.java
+++ src/recurrsion/combinationSum.java
@@ -12,14 +12,20 @@ public class combinationSum {
 
         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
         System.out.println(Arrays.toString(arr));
-        System.out.println("enter the target:-");
+        System.out.print("enter the target:-");
         int target = sc.nextInt();
 
 
         combinationSum ob = new combinationSum();
-        List<List<Integer>> result = ob.combinationS(arr, target);
+        List<List<Integer>> result1 = ob.combinationS(arr, target);
+        List<List<Integer>> result2 = ob.combinationSum2(arr, target);
 
-        for (List<Integer> it : result) {
+        for (List<Integer> it : result1) {
+            System.out.print(it);
+        }
+        System.out.println();
+        System.out.println("ans for combination sum 2:");
+        for (List<Integer> it : result2) {
             System.out.print(it);
         }
         sc.close();
@@ -48,5 +54,33 @@ public class combinationSum {
 
         // not pick case
         findCombinations(ind + 1, arr, target, ans, ds);
+    }
+
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinations2(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+    }
+
+    public static void findCombinations2(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+
+            if (target == 0) {
+                ans.add(new ArrayList<>(ds));
+                return;
+            }
+
+
+        // pick case
+        for (int i = ind; i < arr.length; i++) {
+            if (i > ind && arr[i] == arr[i - 1]) continue;
+            if (arr[i] > target) break;
+
+            ds.add(arr[i]);
+            findCombinations2(i + 1, arr, target - arr[i], ans, ds);
+            ds.remove(ds.size() - 1); // for removing last value in the backtracking
+        }
+
+
     }
 }
