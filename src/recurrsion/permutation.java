3abc7cf4f9fb2e53f5330bb0374b9c9a976a4318
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-05 23:49:31 +0530

    permutations

---
+++ src/recurrsion/permutation.java
@@ -0,0 +1,52 @@
+package recurrsion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class permutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        System.out.println(Arrays.toString(arr));
+        List<List<Integer>> ans = new ArrayList<>();
+        ans = permut(arr);
+        for (List<Integer> it : ans) {
+            for (Integer auto : it) {
+                System.out.print(auto + " ");
+
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+
+    public static List<List<Integer>> permut(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        ArrayList<Integer> ds = new ArrayList<>();
+        permutation(nums, freq, ans, ds);
+        return ans;
+    }
+
+    public static void permutation(int[] nums, boolean[] freq, List<List<Integer>> ans, ArrayList ds) {
+        if (ds.size() == nums.length) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!freq[i]) {
+                freq[i] = true;
+                ds.add(nums[i]);
+                permutation(nums, freq, ans, ds);
+                ds.remove(ds.size() - 1);
+                freq[i] = false;
+            }
+        }
+    }
+}
